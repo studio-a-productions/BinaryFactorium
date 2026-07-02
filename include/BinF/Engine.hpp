fa9f2c016926a4ec61408f64480d17411b7b3157
Alexander Siebens <alexander.siebens@icloud.com>
2026-07-02 20:18:32 +0200

    renderer_backend implemented

--- include/BinF/Engine.hpp
+++ include/BinF/Engine.hpp
@@ -19,24 +19,23 @@ namespace BinF::Engine {
     void Exit();    // free engine alloc resources (reserved for potential desktop)
 
     // RENDER SYSTEM
-    void ClearScreen();
-    void DrawSprite(const screen_pos x, const screen_pos y, const SpriteData&);
-    void DrawSprite(SpritePos*, const SpriteData*);
-    void DrawSprite(const screen_pos x, const screen_pos y, const colourID* sprite,
-        const screen_pos sprite_sx, const screen_pos sprite_sy);
-    void DrawImage(const screen_pos x, const screen_pos y, const colour*image, 
-        const screen_pos image_sx, const screen_pos image_sy);
+    void ClearFrame(const colour col = black); // clear frame buffer (optional)
+    void DrawSprite(const SpritePos&, const SpriteData&);
+    void DrawSprite(screen_pos x, screen_pos y, const SpriteData&); // manual sprite pos
+    void DrawSprite(screen_pos x, screen_pos y, const colourID* sprite, screen_pos s); // s is the size of both axis
+    void DrawSprite(screen_pos x, screen_pos y, const colourID* sprite,
+        screen_pos sprite_sx, screen_pos sprite_sy); // basically manual sprite data
     void PushFrame();
 
     // INPUT SYSTEM
-    bool ButtonPressed(const keycode);  // no "released" variant, just use a simple "!"
-    bool ButtonDown(const keycode);     // pressed this frame?
-    bool ButtonUp(const keycode);       // released this frame?
-    s16  JoystickX();                   // get the X-axis (SDL-style)
-    s16  JoystickY();                   // get the Y-axis (SDL-style)
+    bool ButtonPressed(keycode);  // no "released" variant, just use a simple "!"
+    bool ButtonDown(keycode);     // pressed this frame?
+    bool ButtonUp(keycode);       // released this frame?
+    s16  JoystickX();             // get the X-axis (SDL-style)
+    s16  JoystickY();             // get the Y-axis (SDL-style)
 
     // TIME SYSTEM
-    Time DeltaTime();                   // time between two updates
-    Time ReqTime();                     // time since last ReqTime() or since Update()
+    Time DeltaTime();             // time between two updates
+    Time ReqTime();               // time since last ReqTime() or since Update()
 
 }
--- include/BinF/Engine/Renderer.hpp
+++ include/BinF/Engine/Renderer.hpp
@@ -7,7 +7,8 @@
 #include "Colour.hpp"
 namespace BinF::Engine {
     using screen_pos = u16;
-    constexpr screen_pos screen_x = 240U;
-    constexpr screen_pos screen_y = 296U;
+
+    constexpr screen_pos screen_x = 296U;
+    constexpr screen_pos screen_y = 240U;
     constexpr screen_pos max_screen_pos = (screen_x < screen_y ? screen_y : screen_x) - 1U;
 }
